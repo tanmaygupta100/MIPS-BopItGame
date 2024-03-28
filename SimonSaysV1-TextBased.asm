2a3d8c19228215e7e043301cf350c44dbe897dda
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2024-03-28 00:59:35 -0700

    Update SimonSaysV1-TextBased.asm
    Restructured core procedures to be called from main, removed the game_loop, and added descriptive summaries in comments.

--- SimonSaysV1-TextBased.asm
+++ SimonSaysV1-TextBased.asm
@@ -1,5 +1,6 @@
-# Text-Based V2 - Simon Says
+# Text-Based V2.2 - Bop It
 # Tanmay Gupta
+# TO DO: Nest procedures
 
 .data
 	prompt:		.asciiz "\n Enter your sequence (1-4): "
@@ -7,39 +8,56 @@
 	clrScreen:		.asciiz "\n\n\n\n\n\n\n\n\n\n"
 	winMsg:		.asciiz "You win!\n"
 	loseMsg:		.asciiz "You lose...\n"
-	invalidMsg:	.asciiz "Invalid input!"
+	invalidMsg:	.asciiz "Invalid input, try again!"
 
 
 .text
 
+# Procedure - main:
+# Runs the program and calls the appropriate procedures.
 main:
-	jal game_loop
-
-	# Ends program:
-	li $v0, 10
-	syscall
-
-
-game_loop:
-	# Print lines to "clear" screen:
-	li $v0, 4
-	la $a0, clrScreen
-	syscall
+	jal clear_screen
 
 	jal random_number
 	jal user_prompt
-	j game_loop
+	
+	jal comparison_logic
+	jal num_shifter
+	
+	j main
 
 
+# Procedure - random_number:
+# Generates a random number using syscall 42.
+# Stores the number in saved register s0 for later use.
 random_number:
-	li $a1, 4			#Here you set $a1 to the max bound.
-	li $v0, 42			#generates the random number.
+	li $a1, 4			# Sets $a1 to the max bound.
+	li $v0, 42			# Generates the random number.
 	syscall
-	add $a0, $a0, 1		#Here you add the lowest bound
-	li $v0, 1			#1 print integer
+	add $a0, $a0, 1		# Adds the lowest bound.
+	move $s0, $a0		# Moves random number to s0, to use later.
+	li $v0, 1			# 1 to print integer.
 	syscall
 	jr $ra
 
+# Procedure - num_shifter:
+# Shifts the randomly generated number for updating the sequences.
+num_shifter:
+	jr $ra
+
+
+# Procedure - comparison_logic
+# Compares user inputs to the generated sequences.
+# TO ADD: checks length of sequence, if reached 6, then winner.
+comparison_logic:
+	bne $s0, $s1, loser
+	jr $ra
+
+
+# Procedure - user_prompt
+# Prints messages to the user.
+# Prompts the user for values and accepts inputs.
+# Stores inputs to s1 for later use.
 user_prompt:
 	# Print prompt:
 	li $v0, 4
@@ -60,22 +78,56 @@ user_prompt:
 	# Print the input:
 	li $v0, 1
 	move $a0, $t0
+	move $s1, $a0	# Moves random number to s1, to use later.
 	syscall
 
 	j new_line
 
 	jr $ra
 
-invalid_input:
-	# Print message for invalid input:
-	li $v0, 4
-	la $a0, invalidMsg
-	syscall
-	j user_prompt		# Prompt again for input
 
+# Procedure - new_line:
+# Prints 1 empty line to make outputs easier to separate and read.
 new_line:
-	# Print new line:
 	li $v0, 4
 	la $a0, newLine
 	syscall
 	jr $ra
+# Procedure - clear_screen:
+# Prints empty lines to "clear" the screen.
+clear_screen:
+	li $v0, 4
+	la $a0, clrScreen
+	syscall
+	jr $ra
+
+
+# Procedure - invalid_input:
+# Prints a message indicating that the user inputted an invalid value.
+# Calls the user_prompt to allow the user to try again, without ending the game.
+invalid_input:
+	li $v0, 4
+	la $a0, invalidMsg
+	syscall
+	j user_prompt
+# Procedure - loser:
+# Prints the losing message if any sequence doesn't match..
+# Exits the program.
+loser:
+	li $v0, 4
+	la $a0, loseMsg
+	syscall
+	jal exit_program
+# Procedure - winner:
+# Prints the winning message if 5 sequences are correct in a row.
+# Exits the program.
+winner:
+	li $v0, 4
+	la $a0, winMsg
+	syscall
+	jal exit_program
+# Procedure - exit_program:
+# Loads the syscall 10 to invoke exiting the program.
+exit_program:
+	li $v0, 10
+	syscall
